1379c534b2d363d462322f9379b3a09bfe326678
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-09 00:45:27 +0100

    Create USer Controller function create User

--- src/main/java/com/example/Ecommerce/Controller/UserController.java
+++ src/main/java/com/example/Ecommerce/Controller/UserController.java
@@ -1,4 +1,21 @@
 package com.example.Ecommerce.Controller;
 
+import com.example.Ecommerce.Model.USERR;
+import com.example.Ecommerce.Service.User.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/User")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping()
+    public USERR createUser(@RequestBody USERR userr){
+        return userService.createUser(userr);
+    }
 }
