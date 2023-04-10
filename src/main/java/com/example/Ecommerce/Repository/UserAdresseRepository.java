d023491d25ce5bff7ac9fceba01f42d0e17a905a
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-10 16:59:52 +0100

    UserAdresseRepository.java

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -1,9 +1,9 @@
 package com.example.Ecommerce.Repository;
 
-import com.example.Ecommerce.Model.USER_ADRESSE;
+import com.example.Ecommerce.Model.USERADRESSE;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAdresseRepository extends JpaRepository<USER_ADRESSE,Long> {
+public interface UserAdresseRepository extends JpaRepository<USERADRESSE,Long> {
     //USER_ADRESSE findByUserr_IdUser(Long IdUser);
 
 }
