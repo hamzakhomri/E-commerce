2c2b1260ba8bb1d140a587b8127e982a4a31e3f4
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-11 02:15:33 +0100

    Assign UserAdresseController

--- src/main/java/com/example/Ecommerce/Controller/UserAdresseController.java
+++ src/main/java/com/example/Ecommerce/Controller/UserAdresseController.java
@@ -5,11 +5,26 @@ import com.example.Ecommerce.Services.UserAdresse.IUserAdresse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/useradresse")
 public class UserAdresseController {
      @Autowired
     IUserAdresse iUserAdresse;
+     @PostMapping("/user/{idUser}")
+     public Useradresse assignnewToUserAdresse(@PathVariable("idUser")Long idUser,@RequestBody Useradresse useradresse){
+         return iUserAdresse.assign(idUser,useradresse);
+     }
+
+
+
+
+
+
+
+
+
      @PostMapping()
     public Useradresse save(@RequestBody Useradresse useradresse){
          return iUserAdresse.create(useradresse);
@@ -17,6 +32,11 @@ public class UserAdresseController {
      @GetMapping("/{idUserAdresse}")
     public Useradresse GetOneById(@PathVariable(name = "idUserAdresse")Long idUserAdresse){
          return iUserAdresse.getOneByID(idUserAdresse);
-
      }
+
+     @GetMapping()
+    public List<Useradresse> getAll(){
+         return iUserAdresse.getlAll();
+     }
+
 }
