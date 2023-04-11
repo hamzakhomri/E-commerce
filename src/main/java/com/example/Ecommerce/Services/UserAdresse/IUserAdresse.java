927add4dbb6f102e33df529b6463f48e6c6eb6e8
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-11 02:15:05 +0100

    Assign IUserAdresse.java

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresse.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresse.java
@@ -2,7 +2,11 @@ package com.example.Ecommerce.Services.UserAdresse;
 
 import com.example.Ecommerce.Model.Useradresse;
 
+import java.util.List;
+
 public interface IUserAdresse {
     Useradresse create(Useradresse useradresse);
     Useradresse getOneByID(Long idUserAdresse);
+    List<Useradresse> getlAll();
+    Useradresse assign (Long idUser,Useradresse useradresse);
 }
