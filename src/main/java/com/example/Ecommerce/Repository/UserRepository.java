c63f2eef27988002caf21bc5c7c5281ebe4be48c
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-11 02:16:28 +0100

    Assign UserrRepository.java

--- src/main/java/com/example/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Userr;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<Userr,Long> {
+
 }
