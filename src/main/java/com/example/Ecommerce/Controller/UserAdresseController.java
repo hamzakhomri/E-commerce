b4c547614a8db128945052c731f6c674567420f6
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-13 23:00:36 +0100

    Cleaning UserAdresseController.java For assign

--- src/main/java/com/example/Ecommerce/Controller/UserAdresseController.java
+++ src/main/java/com/example/Ecommerce/Controller/UserAdresseController.java
@@ -1,7 +1,6 @@
 package com.example.Ecommerce.Controller;
 
 import com.example.Ecommerce.Model.Useradresse;
-import com.example.Ecommerce.Services.USer.UserService;
 import com.example.Ecommerce.Services.UserAdresse.IUserAdresseService;
 import com.example.Ecommerce.Services.UserAdresse.UserAdresseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +16,10 @@ public class UserAdresseController {
      @Autowired
     private UserAdresseService userAdresseService;
 
-
-
-
-
-
-     @PostMapping("/user/{idUser}")
-     public Useradresse assignnewToUserAdresse(@PathVariable("idUser")Long idUser,@RequestBody Useradresse useradresse){
-         return iUserAdresseService.assign(idUser,useradresse);
-     }
      @PostMapping()
     public Useradresse save(@RequestBody Useradresse useradresse){
          return iUserAdresseService.create(useradresse);
      }
-
-
 
     @GetMapping()
     public List<Useradresse> getAll(){
@@ -42,17 +30,5 @@ public class UserAdresseController {
     public Useradresse GetOneById(@PathVariable(name = "idUserAdresse")Long idUserAdresse){
          return iUserAdresseService.getOneByID(idUserAdresse);
      }
-
-    @GetMapping("/getUser/{idUser}")
-    public List<Useradresse> getUserByIdUSerAdresse(@PathVariable(required = false)Long idUser){
-         return userAdresseService.getUseradresseByIdUser(idUser);
-    }
-
-
-
-
-
-
-
 
 }
