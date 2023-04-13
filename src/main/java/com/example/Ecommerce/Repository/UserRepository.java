35204731e5aa41458132256951c8b4a832764143
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-13 23:01:27 +0100

    Cleaning UserRepository.java For assign

--- src/main/java/com/example/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Userr;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<Userr,Long> {
 
