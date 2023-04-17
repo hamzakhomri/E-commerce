e5b417ed94c5498342fde2ebbb21916eb8d620bc
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-17 01:17:34 +0100

    Assign User Adresse into user UserAdresseService.java

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/UserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/UserAdresseService.java
@@ -1,6 +1,7 @@
 package com.example.Ecommerce.Services.UserAdresse;
 
 import com.example.Ecommerce.Model.Useradresse;
+import com.example.Ecommerce.Model.Userr;
 import com.example.Ecommerce.Repository.UserAdresseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,12 @@ public class UserAdresseService implements IUserAdresseService {
     @Override
     public List<Useradresse> getlAll() {
         return userAdresseRepository.findAll();
+    }
+
+    @Override
+    public Useradresse assign(Long idUser, Useradresse useradresse) {
+        useradresse.setUserr(new Userr(idUser));
+        return userAdresseRepository.save(useradresse);
     }
 
 
