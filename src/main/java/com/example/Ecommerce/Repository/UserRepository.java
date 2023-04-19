79cf3b124bbdc95e8d70f95b1333ddc5b7a37189
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-19 01:54:30 +0100

    FIlter User By UserName UserRepository

--- src/main/java/com/example/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
 public interface UserRepository extends JpaRepository<Userr,Long> {
+    Optional<Userr> findByNameUserStartsWith(String nameUser);
+
 
 }
