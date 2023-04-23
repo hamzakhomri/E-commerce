4ca50a92d437bc58b954bf705ca23411b4de6a30
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-23 21:52:28 +0100

    Verify if user axsist or not UserAdresseRepository

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserAdresseRepository extends JpaRepository<Useradresse,Long> {
-
+    boolean existsByUserr_IdUser(Long idUser);
 }
