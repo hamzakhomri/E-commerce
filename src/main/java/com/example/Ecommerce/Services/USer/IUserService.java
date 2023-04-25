4ecab59698bf8bf1fc58da44ec0328f482255e8f
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-25 19:33:31 +0100

    IUserService Update USerAdresse NB: Don't work/ if Update Useradresse removed fromUserr

--- src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
+++ src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
@@ -1,15 +1,16 @@
 package com.example.Ecommerce.Services.USer;
 
+import com.example.Ecommerce.Model.Useradresse;
 import com.example.Ecommerce.Model.Userr;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IUserService {
+    Userr update(Long IdUser , Long idUserAdresse, Userr userr,Useradresse useradresse);
     Userr getOneUserByID(Long IdUser);
     Userr getOneBynameUser(String nameUser);
     Userr create(Userr userr);
     List<Userr> getAllUer();
-    Userr update(Long IdUser, Userr userr);
     void deleteUser(Long IdUser);
 }
