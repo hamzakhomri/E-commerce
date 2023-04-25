8e81a06682fe2d9a8d0c66a939aa441031db95a9
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-25 20:01:26 +0100

    Update User : IUserService

--- src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
+++ src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IUserService {
-    Userr update(Long IdUser , Long idUserAdresse, Userr userr,Useradresse useradresse);
+    Userr update(Long IdUser , Userr userr);
     Userr getOneUserByID(Long IdUser);
     Userr getOneBynameUser(String nameUser);
     Userr create(Userr userr);
