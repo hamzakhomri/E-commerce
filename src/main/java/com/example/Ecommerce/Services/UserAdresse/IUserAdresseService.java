8cde19fc189acabcbd1d263bc765a9a6e4e9e8b9
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-25 23:18:33 +0100

    IUserAdresseService UserAdresse Modified but userId null

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
@@ -1,11 +1,10 @@
 package com.example.Ecommerce.Services.UserAdresse;
 
 import com.example.Ecommerce.Model.Useradresse;
-
 import java.util.List;
 
 public interface IUserAdresseService {
-    Useradresse update(Long idUserAdresse,Useradresse useradresse);
+    Useradresse update( Long idUser, Useradresse useradresse);
     Useradresse create(Useradresse useradresse);
     Useradresse getOneByID(Long idUserAdresse);
     List<Useradresse> getlAll();
