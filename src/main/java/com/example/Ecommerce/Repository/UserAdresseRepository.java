30c091c498586cf8d2b158bf36d7924f108de6fa
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-26 17:39:39 +0100

    UserAdresseRepository.java : GetUSerAdresse By UserNme Error 500

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -3,8 +3,19 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Useradresse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserAdresseRepository extends JpaRepository<Useradresse,Long> {
     boolean existsByUserr_IdUser(Long idUser);
 
-    Useradresse findByUserr_NameUserLike(String nameUser);
+    //List<Useradresse> findByUserr_NameUserLike(String nameUser);
+
+    Optional<Useradresse> findByUserr_NameUserLike(String nameUser);
+
+    //Useradresse findByUserr_NameUserLike(String nameUser);
+
+    //Optional<Useradresse> findByUserr_NameUserLike(String nameUser);
+
+
 }
