a37986fd145eba77966d5b7d24b9f7edc47e0489
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-26 15:20:11 +0100

    IUserService.java : GetUSerAdresse By UserNme

--- src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
+++ src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
@@ -1,10 +1,8 @@
 package com.example.Ecommerce.Services.USer;
 
-import com.example.Ecommerce.Model.Useradresse;
 import com.example.Ecommerce.Model.Userr;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IUserService {
     Userr update(Long IdUser , Userr userr);
