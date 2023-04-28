4ec52998ab0e8d72f470cdf75a63c69935368200
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 20:37:24 +0100

    Creation Products and ProductCategory : ProductService.java

---
+++ src/main/java/com/example/Ecommerce/Services/Product/ProductService.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce.Services.Product;
+
+import com.example.Ecommerce.Model.Product;
+import com.example.Ecommerce.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProductService {
+
+}
