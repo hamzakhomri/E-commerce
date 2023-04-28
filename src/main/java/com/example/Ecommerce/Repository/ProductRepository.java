a13f1a3c2fd614c53744f22521f1331c0b0c1aef
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 21:54:22 +0100

    Product : ProductRepository.java

--- src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
@@ -3,5 +3,12 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    boolean existsByIdProducts(Long idProducts);
+    Optional<Product> findByNameProductsStartsWith(String nameProducts);
+    Optional<Product> findByCreated_atUser(LocalDateTime created_Product);
 }
