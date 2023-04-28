166b284e38b01f618ba9558607f16b025a7cc5a9
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 21:54:44 +0100

    Product : ProductService.java

--- src/main/java/com/example/Ecommerce/Services/Product/ProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/ProductService.java
@@ -20,8 +20,43 @@ public class ProductService implements IProductService{
         return productRepository.save(product);
     }
 
+
+    @Override
+    public void deleteProductByID(Long idProducts) {
+            productRepository.deleteById(idProducts);
+    }
+
+    @Override
+    public Product updateProduct(Long idProducts, Product product) {
+        if (!IfExistProduct(idProducts)){
+            product.setIdProducts(idProducts);
+            product.setModified_atUser(LocalDateTime.now());
+            return product;
+        }else {
+            return null;
+        }
+    }
+
+
+    //======================== GET ====================================
+    public boolean IfExistProduct(Long idProducts){
+        return productRepository.existsByIdProducts(idProducts);
+    }
     @Override
     public List<Product> GetAll() {
         return productRepository.findAll();
+    }
+    @Override
+    public Product GetById(Long idProducts,Product product) {
+        return productRepository.findById(idProducts).orElse(null);
+    }
+
+    @Override
+    public Product GetByName( String nameProducts,Product product) {
+        return productRepository.findByNameProductsStartsWith(nameProducts).orElse(new Product());
+    }
+    @Override
+    public Product GetByDateCreated(LocalDateTime created_Product,Product product){
+        return productRepository.findByCreated_atUser(created_Product).orElse(null);
     }
 }
