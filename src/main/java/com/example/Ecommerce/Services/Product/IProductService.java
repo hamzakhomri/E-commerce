7764ec71e2ba5de6bb914f43914d65cb54c3b0de
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 21:53:47 +0100

    Product : IProductService.java

--- src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
@@ -1,12 +1,19 @@
 package com.example.Ecommerce.Services.Product;
 
 import com.example.Ecommerce.Model.Product;
-import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IProductService {
     Product createProduct(Product product);
+    void deleteProductByID(Long idProducts);
+    Product updateProduct(Long idProducts,Product product);
+
+    //======================== GET ====================================
     List<Product> GetAll();
+    Product GetById(Long idProducts,Product product);
+    Product GetByName(String nameProducts,Product product);
+    Product GetByDateCreated(LocalDateTime created_Product,Product product);
 
 }
