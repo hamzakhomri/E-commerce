daf41de400a67bfec6d84c904a7d91b4c30c0a14
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 20:40:30 +0100

    Rename

--- src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.example.Ecommerce.Repository;
 
-import com.example.Ecommerce.Model.Products;
+import com.example.Ecommerce.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Products,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
