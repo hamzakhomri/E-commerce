ebb2450c2b1758407e13c9d9f5a6fd8241f974a7
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 20:36:32 +0100

    Creation Products and ProductCategory : IProductService.java

---
+++ src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
@@ -0,0 +1,8 @@
+package com.example.Ecommerce.Services.Product;
+
+import com.example.Ecommerce.Model.Product;
+import org.springframework.stereotype.Service;
+
+public interface IProductService {
+
+}
