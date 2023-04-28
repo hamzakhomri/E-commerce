4197eb852bb8cc3a85f8ad9f92412484312d590c
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-28 20:46:42 +0100

    Create Product : IProductService.java

--- src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
@@ -3,6 +3,10 @@ package com.example.Ecommerce.Services.Product;
 import com.example.Ecommerce.Model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface IProductService {
+    Product createProduct(Product product);
+    List<Product> GetAll();
 
 }
