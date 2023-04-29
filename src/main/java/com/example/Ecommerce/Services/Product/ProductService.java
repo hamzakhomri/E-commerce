079aed2b6945ffb0a94f53cd952a67f5e6081885
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-29 20:03:54 +0100

    Dones

--- src/main/java/com/example/Ecommerce/Services/Product/ProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/ProductService.java
@@ -4,6 +4,7 @@ import com.example.Ecommerce.Model.Product;
 import com.example.Ecommerce.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,23 +16,30 @@ public class ProductService implements IProductService{
 
     @Override
     public Product createProduct(Product product){
-        product.setCreated_atUser(LocalDateTime.now());
-        product.setModified_atUser(LocalDateTime.now());
-        return productRepository.save(product);
+        product.setCreated_atProduct(LocalDateTime.now());
+        product.setModified_atProduct(LocalDateTime.now());
+        return this.productRepository.save(product);
     }
-
+    public boolean IfExistProduct(Long idProducts){
+        return productRepository.existsByIdProducts(idProducts);
+    }
 
     @Override
     public void deleteProductByID(Long idProducts) {
             productRepository.deleteById(idProducts);
     }
 
+    @Transactional
     @Override
     public Product updateProduct(Long idProducts, Product product) {
-        if (!IfExistProduct(idProducts)){
+        if (IfExistProduct(idProducts)==true){
+
+            Product old = productRepository.findById(idProducts).get();
+            //========= WhatCan I UPdated ===================
             product.setIdProducts(idProducts);
-            product.setModified_atUser(LocalDateTime.now());
-            return product;
+            product.setModified_atProduct(LocalDateTime.now());
+            product.setCreated_atProduct(old.getCreated_atProduct());
+            return productRepository.save(product);
         }else {
             return null;
         }
@@ -39,24 +47,22 @@ public class ProductService implements IProductService{
 
 
     //======================== GET ====================================
-    public boolean IfExistProduct(Long idProducts){
-        return productRepository.existsByIdProducts(idProducts);
-    }
+
     @Override
     public List<Product> GetAll() {
         return productRepository.findAll();
     }
     @Override
-    public Product GetById(Long idProducts,Product product) {
+    public Product GetById(Long idProducts) {
         return productRepository.findById(idProducts).orElse(null);
     }
 
     @Override
-    public Product GetByName( String nameProducts,Product product) {
+    public Product GetByName( String nameProducts) {
         return productRepository.findByNameProductsStartsWith(nameProducts).orElse(new Product());
     }
-    @Override
-    public Product GetByDateCreated(LocalDateTime created_Product,Product product){
-        return productRepository.findByCreated_atUser(created_Product).orElse(null);
-    }
+   // @Override
+    //public Product GetByDateCreated(LocalDateTime created_Product){
+     //   return productRepository.findByCreated_atUser(created_Product).orElse(null);
+    //}
 }
