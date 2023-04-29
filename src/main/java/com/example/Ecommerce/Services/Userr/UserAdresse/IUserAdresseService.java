b5252c9c03b6cd8868d1539ef6680ea2343e4c3c
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-29 20:03:25 +0100

    Dones

--- src/main/java/com/example/Ecommerce/Services/Userr/UserAdresse/IUserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/Userr/UserAdresse/IUserAdresseService.java
@@ -1,11 +1,13 @@
 package com.example.Ecommerce.Services.Userr.UserAdresse;
 
 import com.example.Ecommerce.Model.User.Useradresse;
+import com.example.Ecommerce.Model.User.Userr;
+
 import java.util.List;
 
 public interface IUserAdresseService {
+    Useradresse update(Long idUserAdresse,Useradresse useradresse);
     Useradresse findByUserr_NameUserLike(String nameUser);
-    Useradresse update( Long idUser, Useradresse useradresse);
     Useradresse create(Useradresse useradresse);
     Useradresse getOneByID(Long idUserAdresse);
     List<Useradresse> getlAll();
