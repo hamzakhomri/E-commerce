a99977d1084296235dc4e92f630ec60d9a24cccc
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-29 20:04:03 +0100

    Dones

--- src/main/java/com/example/Ecommerce/Model/User/Useradresse.java
+++ src/main/java/com/example/Ecommerce/Model/User/Useradresse.java
@@ -14,14 +14,11 @@ public class Useradresse {
     private Long idUserAdresse;
     private String nameUserAdresse;
 
-    //private String adresseline1UserAdresse;
-    //private String  cityUserAdresse;
-    //private Integer postalcodeUserAdresse;
-    //private String countryUserAdresse;
+
+
     public String getNameUserAdresse() {
         return nameUserAdresse;
     }
-
     public void setNameUserAdresse(String nameUserAdresse) {
         this.nameUserAdresse = nameUserAdresse;
     }
