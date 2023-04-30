7369d530183d157e1daa1a41ded82da5bb84dd0e
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-30 15:27:56 +0100

    Find UserAdresee with UserName

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
@@ -1,14 +1,20 @@
 package com.example.Ecommerce.Services.UserAdresse;
 
 import com.example.Ecommerce.Model.Useradresse;
+import com.example.Ecommerce.Model.Userr;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserAdresseService {
-    Useradresse update(Long idUserAdresse,Useradresse useradresse);
-    Useradresse findByUserr_NameUserLike(String nameUser);
-    Useradresse create(Useradresse useradresse);
+
+    //°°°°°°°°°°°°°°°°°°°°°°°°°°°°° GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
     Useradresse getOneByID(Long idUserAdresse);
     List<Useradresse> getlAll();
+    List<Useradresse> getUserAdressesByUserName(String name);
+
+    //°°°°°°°°°°°°°°°°°°°°°°°°°°° END GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+    Useradresse update(Long idUserAdresse,Useradresse useradresse);
+    Useradresse create(Useradresse useradresse);
     Useradresse assign(Long idUser,Useradresse useradresse);
 }
