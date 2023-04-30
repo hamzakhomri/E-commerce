96f5f404d6b0d0c2b426fe6ad41d26015c9b18f3
hamzakhomri <hamza.khomri36@gmail.com>
2023-04-30 15:28:10 +0100

    Find UserAdresee with UserName

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -3,18 +3,11 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Useradresse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserAdresseRepository extends JpaRepository<Useradresse,Long> {
     boolean existsByUserr_IdUser(Long idUser);
-
-    //List<Useradresse> findByUserr_NameUserLike(String nameUser);
-
-    Optional<Useradresse> findByUserr_NameUserLike(String nameUser);
-
-    //Useradresse findByUserr_NameUserLike(String nameUser);
-
-    //Optional<Useradresse> findByUserr_NameUserLike(String nameUser);
-
+    List<Useradresse> findByUserrNameUser(String nameUser);
 
 }
