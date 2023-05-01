5f77c1dda43cac985836ac153216cd63890b97cc
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-01 19:06:28 +0100

    Done Request Product & Userr

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
@@ -1,20 +1,18 @@
 package com.example.Ecommerce.Services.UserAdresse;
 
 import com.example.Ecommerce.Model.Useradresse;
-import com.example.Ecommerce.Model.Userr;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IUserAdresseService {
 
     //°°°°°°°°°°°°°°°°°°°°°°°°°°°°° GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
     Useradresse getOneByID(Long idUserAdresse);
     List<Useradresse> getlAll();
-    List<Useradresse> getUserAdressesByUserName(String name);
+    List<Useradresse> findByUserr_NameUserStartsWith(String nameUser);
 
     //°°°°°°°°°°°°°°°°°°°°°°°°°°° END GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
     Useradresse update(Long idUserAdresse,Useradresse useradresse);
     Useradresse create(Useradresse useradresse);
-    Useradresse assign(Long idUser,Useradresse useradresse);
+    Useradresse assign(Long idUser, Useradresse useradresse);
 }
