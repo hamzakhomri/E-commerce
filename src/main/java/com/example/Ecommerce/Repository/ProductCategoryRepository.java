179f720104c2f9c34428829fde75f23bcc616524
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 17:50:29 +0100

    Create Product Category Next Step : Assign Product To Product Category

--- src/main/java/com/example/Ecommerce/Repository/ProductCategoryRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductCategoryRepository.java
@@ -4,5 +4,4 @@ import com.example.Ecommerce.Model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
-
 }
