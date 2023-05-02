93b8a3b1eda6954750bc815f568d9834762b7dfa
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 15:24:40 +0100

    Find By Name return null But Not Filter

--- src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
+++ src/main/java/com/example/Ecommerce/Services/USer/IUserService.java
@@ -5,11 +5,12 @@ import com.example.Ecommerce.Model.Userr;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserService {
     Userr update(Long IdUser , Userr userr);
     Userr getOneUserByID(Long IdUser);
-    Userr getOneBynameUser(String nameUser);
+    List<Optional<Userr>> getOneBynameUser(String nameUser);
     Userr create(Userr userr) throws UnknownHostException, SocketException;
     List<Userr> getAllUer();
     void deleteUser(Long IdUser);
