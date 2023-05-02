3b2b396dad288d4e2351f3a0c340bc9f38cc319e
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 15:25:19 +0100

    Find By Name return null But Not Filter

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserAdresseRepository extends JpaRepository<Useradresse,Long> {
+
+    List<Optional<Useradresse>> findByUserr_NameUserStartsWith(String nameUser);
     boolean existsByUserr_IdUser(Long idUser);
-    List<Useradresse> findByUserr_NameUserStartsWith(String nameUser);
-    //List<Useradresse> findByUserrNameUser(String nameUser);
 }
