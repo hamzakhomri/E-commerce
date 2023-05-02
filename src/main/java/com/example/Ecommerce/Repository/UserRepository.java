4524fe968dc05805133a2b70a5256a7f08d7a038
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 15:24:55 +0100

    Find By Name return null But Not Filter

--- src/main/java/com/example/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Userr;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Userr,Long> {
-    Optional<Userr> findByNameUserStartsWith(String nameUser);
+
+    List<Optional<Userr>> findByNameUserStartsWith(String nameUser);
     boolean existsByIdUser(Long idUser);
 
 }
