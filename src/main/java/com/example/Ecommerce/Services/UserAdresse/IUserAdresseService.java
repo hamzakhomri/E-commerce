1f3b8329a1ac5d55df328f01f8913a9b5becab06
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 15:24:34 +0100

    Find By Name return null But Not Filter

--- src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
+++ src/main/java/com/example/Ecommerce/Services/UserAdresse/IUserAdresseService.java
@@ -3,13 +3,14 @@ package com.example.Ecommerce.Services.UserAdresse;
 import com.example.Ecommerce.Model.Useradresse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserAdresseService {
 
     //°°°°°°°°°°°°°°°°°°°°°°°°°°°°° GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
     Useradresse getOneByID(Long idUserAdresse);
     List<Useradresse> getlAll();
-    List<Useradresse> findByUserr_NameUserStartsWith(String nameUser);
+    List<Optional<Useradresse>> findByUserr_NameUserStartsWith(String nameUser);
 
     //°°°°°°°°°°°°°°°°°°°°°°°°°°° END GET °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
     Useradresse update(Long idUserAdresse,Useradresse useradresse);
