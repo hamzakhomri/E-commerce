0392dc1211326ab893c457a36090a0e4048a3add
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-02 17:50:34 +0100

    Create Product Category Next Step : Assign Product To Product Category

---
+++ src/main/java/com/example/Ecommerce/Services/ProductCartegory/ProductCategoryService.java
@@ -0,0 +1,35 @@
+package com.example.Ecommerce.Services.ProductCartegory;
+
+import com.example.Ecommerce.Model.Product;
+import com.example.Ecommerce.Model.ProductCategory;
+import com.example.Ecommerce.Repository.ProductCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+@Service
+public class ProductCategoryService implements IProductCategoryService{
+
+    @Autowired
+    ProductCategoryRepository productCategoryRepository;
+
+
+    @Override
+    public List<ProductCategory> GetAll() {
+        return productCategoryRepository.findAll();
+    }
+
+    @Override
+    public ProductCategory SaveProductCategory(ProductCategory productCategory) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        productCategory.setCreatedProductCategory(dateTime.format(formatter));
+        productCategory.setModifiedProductCategory(dateTime.format(formatter));
+
+        return this.productCategoryRepository.save(productCategory);
+    }
+
+}
