0f328579a234a0dd43c44cab3f16cf9172d01140
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-04 16:30:06 +0100

    Create and Get ALl Product Picture

--- src/main/java/com/example/Ecommerce/Services/ProductPicture/ProductPicture.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ecommerce.Services.ProductPicture;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductPicture implements IProductPictureService{
-}
