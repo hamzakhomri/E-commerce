463fca78b2ea697cc2abcee3ee9c3d9f4910bc04
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-04 16:30:26 +0100

    Create and Get ALl Product Picture

--- src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
+++ src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
@@ -1,4 +1,10 @@
 package com.example.Ecommerce.Services.ProductPicture;
 
+import com.example.Ecommerce.Model.Productpicture;
+
+import java.util.List;
+
 public interface IProductPictureService {
+    Productpicture create(Productpicture productpicture);
+    List<Productpicture> GetAll();
 }
