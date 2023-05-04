aaa5bd3d292a78e440338d8a26a213c2159cab2d
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-04 15:41:32 +0100

    Creations

---
+++ src/main/java/com/example/Ecommerce/Services/ProductPicture/ProductPicture.java
@@ -0,0 +1,7 @@
+package com.example.Ecommerce.Services.ProductPicture;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductPicture implements IProductPictureService{
+}
