3926da3f9eb0e2bc3078215ba0133382df1e6457
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-04 15:41:45 +0100

    Creations

---
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -0,0 +1,7 @@
+package com.example.Ecommerce.Repository;
+
+import com.example.Ecommerce.Model.ProductPicture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductPictureRepository extends JpaRepository<ProductPicture,Long> {
+}
