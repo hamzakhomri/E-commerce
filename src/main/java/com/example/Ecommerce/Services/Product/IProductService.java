e2a4a75769ceb84a1a2911da1a8c39d71c4db271
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-04 18:26:45 +0100

    d

--- src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
@@ -1,12 +1,14 @@
 package com.example.Ecommerce.Services.Product;
 
 import com.example.Ecommerce.Model.Product;
+import com.example.Ecommerce.Model.ProductCategory;
+import com.example.Ecommerce.Model.Productpicture;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IProductService {
-    Product createProduct(Product product);
+    Product createProduct(Product product, Long idProductpicture);
     void deleteProductByID(Long idProducts);
     Product updateProduct(Long idProducts,Product product);
 
