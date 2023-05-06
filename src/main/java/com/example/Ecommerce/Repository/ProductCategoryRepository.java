cbcb0853d1f67cef8baf3e31c2066572d41207d6
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-06 20:40:19 +0100

    find ProductCategory and ProdcutPicture by IdProductq dont work

--- src/main/java/com/example/Ecommerce/Repository/ProductCategoryRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductCategoryRepository.java
@@ -4,4 +4,5 @@ import com.example.Ecommerce.Model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+    ProductCategory getProductCategoryByProductId(Long idProducts);
 }
