91316d0e75e71ed47657ccea92ff2f076b3aeb6e
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-06 15:30:34 +0100

    Assign Product to Product Picture Done

--- src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
+++ src/main/java/com/example/Ecommerce/Services/Product/IProductService.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface IProductService {
 
-    void assign(Long idProducts, Long idProductCategory);
+    void assignToProductCtegory(Long idProducts, Long idProductCategory);
+    void assignToProductPicture(Long idProducts, Long idProductpicture);
     //Productpicture assign2(Long idProducts, Long idProductpicture);
     Product createProduct(Product product);
     void deleteProductByID(Long idProducts);
