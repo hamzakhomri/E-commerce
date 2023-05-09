0bd75afa2ddb360fa349ee441bf8e287a0e5402b
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-09 18:06:01 +0100

    Get All Product don't Work a gain -_-

--- src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
@@ -2,12 +2,17 @@ package com.example.Ecommerce.Repository;
 
 import com.example.Ecommerce.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.category JOIN FETCH p.picture WHERE p.id = :id")
+    Optional<Product> findByIdWithCategoryAndPicture(@Param("id") Long id);
 
     //===================  FIND TO ASIGNING ===================================
     boolean existsByProductCategory_IdProductCategory(Long idProductCategory);
