02aa027d9049abe9d0e8458a7e4a7f7be0a09414
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-09 14:42:49 +0100

    Remove find ProductCategory and ProdcutPicture by IdProducts

--- src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductPictureRepository extends JpaRepository<Productpicture,Long> {
-    List<Productpicture> getProductPicturesByProductId(Long idProducts);
 }
