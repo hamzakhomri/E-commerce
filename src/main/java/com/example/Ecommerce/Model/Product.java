681fe5e9587ba7e2ad29e6042bfb748f3df03208
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-11 16:40:42 +0100

    Get productCategory=null, Picture In Product but assigign Category to Product Is null

--- src/main/java/com/example/Ecommerce/Model/Product.java
+++ src/main/java/com/example/Ecommerce/Model/Product.java
@@ -1,6 +1,7 @@
 package com.example.Ecommerce.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
@@ -26,12 +27,12 @@ public class Product {
     //==Relation With Product Category
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "idProductCategory")
-    @JsonIgnore
+    @JsonIgnoreProperties
     private ProductCategory productCategory;
 
     //==Relation With Product Picture
     @OneToMany(mappedBy = "product",cascade = CascadeType.ALL)
-    @JsonIgnore
+    @JsonIgnoreProperties
     private List<Productpicture> productpictures=new ArrayList<>();
 
 //°°°°°°°°°°°GETTER AND SETTER °°°°°°°°°°°°°°°°°°°°°°°°°°
