6f8b3afd3fdf7ee613046500b0d256448378b15b
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-12 21:03:54 +0100

    Update and Delete ProductCategory

--- src/main/java/com/example/Ecommerce/Services/ProductCartegory/IProductCategoryService.java
+++ src/main/java/com/example/Ecommerce/Services/ProductCartegory/IProductCategoryService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface IProductCategoryService {
     List<ProductCategory> GetAll();
     ProductCategory SaveProductCategory(ProductCategory productCategory);
+    ProductCategory UpdateProductCategory(Long idProductCategory,ProductCategory productCategory);
+    void deleteProductCategory(Long idProductCategory);
 }
