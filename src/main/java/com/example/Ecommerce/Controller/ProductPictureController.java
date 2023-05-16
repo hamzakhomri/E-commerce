00adbe9210acab4e2c07ba1b0e419df62d1df00d
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-16 17:30:56 +0100

    just for

--- src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
@@ -34,6 +34,6 @@ public class ProductPictureController {
     @DeleteMapping("/{idProductpicture}")
     public String deleteProductPicutre(@PathVariable Long idProductpicture){
         iProductPictureService.deleteProductPicture(idProductpicture);
-        return idProductpicture+": Deleted";s
+        return idProductpicture+": Deleted";
     }
 }
