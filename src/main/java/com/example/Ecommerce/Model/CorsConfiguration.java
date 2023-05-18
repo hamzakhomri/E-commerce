d155d9f12588eef1623123734a77b3ec2585ed20
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-18 20:20:13 +0100

    just for

--- src/main/java/com/example/Ecommerce/Model/CorsConfiguration.java
+++ src/main/java/com/example/Ecommerce/Model/CorsConfiguration.java
@@ -3,6 +3,7 @@ package com.example.Ecommerce.Model;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
