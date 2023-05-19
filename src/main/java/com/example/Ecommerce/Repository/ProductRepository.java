7d9d19e91241947dea3409baf495dd8482e33319
hamzakhomri <hamza.khomri36@gmail.com>
2023-05-19 21:41:24 +0100

    just for

--- src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     //===================  FIND TO ASIGNING ===================================
