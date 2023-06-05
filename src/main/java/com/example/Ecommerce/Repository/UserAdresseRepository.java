f703dbc62adb853f95671e684b808abcd0c41dbe
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-05 16:11:52 +0100

    Just For

--- src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserAdresseRepository.java
@@ -4,7 +4,6 @@ import com.example.Ecommerce.Model.Useradresse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
-
 public interface UserAdresseRepository extends JpaRepository<Useradresse,Long> {
     List<Optional<Useradresse>> findByUserr_NameUserStartsWith(String nameUser);
     boolean existsByUserr_IdUser(Long idUser);
