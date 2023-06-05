507915d14af2703735b6292fa43de689389c32aa
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-05 16:13:30 +0100

    Just For

--- src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -3,7 +3,6 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Product;
 import com.example.Ecommerce.Model.Productpicture;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 public interface ProductPictureRepository extends JpaRepository<Productpicture,Long> {
 }
