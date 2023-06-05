41c01cb951def9ea348823dd35a4bb784e4e3452
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-05 16:09:18 +0100

    Update Category from product

--- src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("productpicture")
 public class ProductPictureController {
