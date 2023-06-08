24e58d24321afef02fa4a2611ee8fd6540f0637d
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-08 20:38:47 +0100

    Just For

--- src/main/java/com/example/Ecommerce/Controller/ProductController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductController.java
@@ -12,14 +12,12 @@ import com.example.Ecommerce.Services.ProductCartegory.ProductCategoryService;
 import com.example.Ecommerce.Services.ProductPicture.ProductPictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/product")
 @CrossOrigin("http://localhost:8082/home")
