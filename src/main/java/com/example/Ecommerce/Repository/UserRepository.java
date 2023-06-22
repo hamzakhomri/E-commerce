f7ba7850726f4c55b387ea67c932764110a3860a
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-22 16:51:40 +0100

    Just FOr

--- src/main/java/com/example/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.Ecommerce.Repository;
 
 import com.example.Ecommerce.Model.Userr;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 public interface UserRepository extends JpaRepository<Userr,Long> {
