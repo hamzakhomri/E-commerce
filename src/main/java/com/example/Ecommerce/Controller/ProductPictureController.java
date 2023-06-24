bc14b4e0a0419521788c7b9fb9a38e481a6e4465
hamzakhomri <hamza.khomri36@gmail.com>
2023-06-24 14:57:52 +0100

    Just For

--- src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
@@ -6,11 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("productpicture")
