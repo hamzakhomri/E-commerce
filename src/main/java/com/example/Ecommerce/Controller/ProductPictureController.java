db26d3d6f1081e9966469d021585a14d76b96202
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-06 17:21:00 +0100

    just For

--- src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
@@ -17,7 +17,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("productpicture")
 @CrossOrigin("http://localhost:8081/Tester")
