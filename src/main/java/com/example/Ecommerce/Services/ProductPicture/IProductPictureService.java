3c679b1e2c22a7e70dcfb39b9cc8b1d7bbb1575e
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-24 15:16:55 +0100

    Shouw count pictures from products

--- src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
+++ src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
@@ -5,6 +5,8 @@ import com.example.Ecommerce.Model.Productpicture;
 import java.util.List;
 
 public interface IProductPictureService {
+
+    long countByProduct_IdProducts(Long idProducts);
     List<Productpicture> GetAll();
     Productpicture findbyid(Long id);
     Productpicture create(Productpicture productpicture);
