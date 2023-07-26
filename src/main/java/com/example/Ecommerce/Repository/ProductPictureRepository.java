0a4ac18b182d51cb84ceed6ffe01d7fa01b235f1
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-26 15:59:58 +0100

    show Picture with ProductId

--- src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -4,6 +4,10 @@ import com.example.Ecommerce.Model.Product;
 import com.example.Ecommerce.Model.Productpicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
+
 public interface ProductPictureRepository extends JpaRepository<Productpicture,Long> {
     long countByProduct_IdProducts(Long idProducts);
+    List <Productpicture> findByProduct_IdProducts(Long idProducts);
+
 }
