f844f9e236b209ac11155f25c5cb3e0b081902c5
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-26 15:59:43 +0100

    show Picture with ProductId

--- src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
+++ src/main/java/com/example/Ecommerce/Services/ProductPicture/IProductPictureService.java
@@ -3,10 +3,13 @@ package com.example.Ecommerce.Services.ProductPicture;
 import com.example.Ecommerce.Model.Productpicture;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductPictureService {
 
     long countByProduct_IdProducts(Long idProducts);
+    List <Productpicture> findByProduct_IdProducts(Long idProducts);
+
     List<Productpicture> GetAll();
     Productpicture findbyid(Long id);
     Productpicture create(Productpicture productpicture);
