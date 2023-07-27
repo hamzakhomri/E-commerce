c3aa5cf8f21f13904e37fffb9e76084038e3eeee
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-27 18:20:04 +0100

    Celaning

--- src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -3,11 +3,13 @@ package com.example.Ecommerce.Repository;
 import com.example.Ecommerce.Model.Product;
 import com.example.Ecommerce.Model.Productpicture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductPictureRepository extends JpaRepository<Productpicture,Long> {
+public interface ProductPictureRepository extends JpaRepository<Productpicture,Long>, JpaSpecificationExecutor<Productpicture> {
+    List <Optional<Productpicture>>findByProduct_IdProducts(Long idProducts);
     long countByProduct_IdProducts(Long idProducts);
-    List <Productpicture> findByProduct_IdProducts(Long idProducts);
 
 }
