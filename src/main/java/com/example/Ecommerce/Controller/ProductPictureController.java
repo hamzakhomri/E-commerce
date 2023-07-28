95747b3f7b4ca610bcfd91ba8c3b4d22a4dcf774
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-28 14:17:31 +0100

    Update getProductpicture using the Files.readAllBytes method and builds a ResponseEntity<byte[]> to return the image data as the response.
                             The headers.setContentType() method sets the appropriate content type for the image. Make sure to adjust the content type based on the actual image type (e.g., MediaType.IMAGE_JPEG for JPEG images, MediaType.IMAGE_PNG for PNG images, etc.).

--- src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
+++ src/main/java/com/example/Ecommerce/Controller/ProductPictureController.java
@@ -5,6 +5,8 @@ import com.example.Ecommerce.Repository.ProductPictureRepository;
 import com.example.Ecommerce.Repository.ProductRepository;
 import com.example.Ecommerce.Services.ProductPicture.IProductPictureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +31,27 @@ public class ProductPictureController {
     @Autowired
     ProductRepository productRepository;
 
+    @GetMapping("/byid/{idProductpicture}")
+    public ResponseEntity<byte[]> getProductpicture(@PathVariable Long idProductpicture) {
+        Optional<Productpicture> productpicture = iProductPictureService.getProductpictureById(idProductpicture);
+        // if (!productpicture) {Use isEmpty() instead of checking for null
+        //  return ResponseEntity.notFound().build();
+        // }
+
+        try {
+            Path imagePath = Paths.get(productpicture.get().getPath());
+            byte[] imageBytes = Files.readAllBytes(imagePath);
+
+            // Set appropriate headers for the image response
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG); // Adjust the content type based on the image type
+
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 
     @GetMapping("/{idProducts}")
@@ -45,17 +68,19 @@ public class ProductPictureController {
 
             String filename = file.getOriginalFilename();
             String extension = filename.substring(filename.lastIndexOf(".") + 1);
-            String extensionname = filename.substring(0, filename.lastIndexOf("."));
+            String extensionName = filename.substring(0, filename.lastIndexOf("."));
             LocalDateTime dateTime = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-            String pathDirectory = "productpicture";
-            File directory = new File(pathDirectory);
+            String baseDirectory = "productpicture";
+            File directory = new File(baseDirectory);
             if (!directory.exists()) {
                 directory.mkdir();
             }
 
-            Path path = Paths.get(pathDirectory, filename);
+            // Generate a unique filename to avoid conflicts
+            String uniqueFilename = extensionName + "_" + System.currentTimeMillis() + "." + extension;
+            Path path = Paths.get(baseDirectory, uniqueFilename);
 
             Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
             System.out.println("Copy Success");
@@ -67,15 +92,16 @@ public class ProductPictureController {
                     return null;
                 }
 
-                String pictureName = extensionname + "_" + file.getSize() + "KO";
-                String picturePath = path.toString();
-                String createdatPicture = dateTime.format(formatter);
+                String pictureName = extensionName + "_" + file.getSize() + "KO";
+                String picturePath = "productpicture/" + uniqueFilename; // Relative path
+
+                String createdAtPicture = dateTime.format(formatter);
 
                 Productpicture productpicture = new Productpicture();
                 productpicture.setNamePicture(pictureName);
                 productpicture.setPath(picturePath);
                 productpicture.setSizePicture(file.getSize());
-                productpicture.setCreatedatPicture(createdatPicture);
+                productpicture.setCreatedatPicture(createdAtPicture);
                 productpicture.setProduct(product);
 
                 System.out.println("SAVE PICTURE SUCCESSFULLY");
@@ -89,11 +115,6 @@ public class ProductPictureController {
         }
         return null;
     }
-
-
-
-
-
 
 
     @GetMapping()
