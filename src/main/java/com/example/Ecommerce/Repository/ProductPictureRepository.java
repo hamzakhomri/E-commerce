2688cfdc4d0e03f5deab285644ce9f75291c177a
hamzakhomri <hamza.khomri36@gmail.com>
2023-07-31 15:37:19 +0100

    Just for

--- src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
+++ src/main/java/com/example/Ecommerce/Repository/ProductPictureRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface ProductPictureRepository extends JpaRepository<Productpicture,Long>, JpaSpecificationExecutor<Productpicture> {
     List <Optional<Productpicture>>findByProduct_IdProducts(Long idProducts);
     long countByProduct_IdProducts(Long idProducts);
